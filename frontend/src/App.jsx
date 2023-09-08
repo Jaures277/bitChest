77377a2835c9d7991d9c99e2c8ffcfd0e4beb121
Jaures277 <okouagostino@gmail.com>
2023-09-08 19:22:50 +0200

    frontend: mise en place du login, et protection des routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,6 +12,17 @@ import {
 } from "react-router-dom";
 import Login from './pages/login/Login';
 import Home from './pages/home/Home';
+import { useSelector } from 'react-redux';
+
+const ProtectedRoute = ({children})=> {
+  const token = useSelector((state) => state.auth.user?.token);
+  console.log(token)
+    if(!token) {
+        return <Navigate to="/login" replace={true} />
+    }
+
+    return children ? children: <Outlet />
+}
 
 function App() {
   const [count, setCount] = useState(0)
@@ -20,7 +31,10 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path='/login' element={<Login/>}/>
-        <Route path='/home' element={<Home/>}/>
+        
+        <Route element={<ProtectedRoute />}>
+          <Route path='/home' element={<Home/>}/>
+        </Route>
       </Routes>
     </BrowserRouter>
   )
